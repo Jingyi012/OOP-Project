2863d892c3c506c279e306c1a1b7dd3cb346266d
Jingyi <jingyi012@gmail.com>
2023-06-25 22:28:45 +0800

    update

--- Doctor.java
+++ Doctor.java
@@ -1,22 +1,12 @@
 import java.util.*;
 public class Doctor extends User{
-  public ArrayList<Appointment> appointments;
-
     public Doctor(String name, String icNum, String DOB, String gender, Address address, String phone, String email) {
          super(name, icNum, DOB, gender, address, phone, email);
-         this.appointments=new ArrayList<>();
     }
-
-    public ArrayList<Appointment> getAppointments(){
-      return appointments;
-    }
-
 
      public void displayInfo(){
         System.out.println("\n-----------Doctor Info-----------");
         super.displayInfo();
     }
-
-   
 
 }
